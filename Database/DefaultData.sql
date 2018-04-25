d48dfbe42828f48af437e89c59a644ef0c4688b1
Adrianne Claude Tubig <adriannetubig@gmail.com>
2018-04-25 15:56:25 +0800

    Database Files
    Updated Sample Data

--- Database/DefaultData.sql
+++ Database/DefaultData.sql
@@ -4,14 +4,3 @@ INSERT INTO campaign(Name)
 VALUES
     ('Flexr/PayPlus/Choice'),
     ('Flexible Outsourcing')
-    
-
-INSERT INTO coachingrecord
-    (FollowUpDate, CampaignId, AgentName, Campaign, CoachingTopic, ActionPlans, AreaOfOpportunity, AreaOfSuccess) VALUES
-(58, 'yes', 'Horizon Outsourcing', 'qe', 'rewr', 'weryter', 'qweq', '2018-04-10'),
-(60, 'no', 'Horizon Outsourcing', 'Juna Juno', 'N/A', 'N/A', 'N/A', '1970-01-01'),
-(61, 'yes', 'Flexr/PayPlus/Choice', 'Juan Dela Cruz', '\"Lorem ipsum dolor sit amet, c', '\"Lorem ipsum dolor sit amet, c', '\"Lorem ipsum dolor sit amet, c', '2018-04-01'),
-(62, 'YES', 'Flexible Outsourcing', 'john cena', '8838383', 'q', 'q', '1970-01-01'),
-(63, 'No', 'Flexible Outsourcing', 'John john', 'n/a', 'N/A', '\"Lorem ipsum dolor sit amet, c', '2018-04-30'),
-(64, 'Yes', 'Horizon Outsourcing', 'Angel Cruz', 'Lorem Ipsum', 'Lorem Ipsum', 'Lorem ipsum', '2018-04-29'),
-(65, 'Yes', 'Horizon Outsourcing', 'qqweq', 'qweqwe', 'qweqw', 'qweqwe', '2018-04-29');
